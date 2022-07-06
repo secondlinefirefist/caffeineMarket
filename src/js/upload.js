b6e1f8240e6ea695d87be2d9e36753368331e52a
BradleyyKim <win930323@gmail.com>
2022-07-06 19:38:29 +0900

    search UI 구현

--- src/js/upload.js
+++ src/js/upload.js
@@ -1,7 +1,24 @@
-const loadFile = (event) => {
+const loadFile = (e) => {
+  // const loadImage = document.querySelector('.loadImageArea');
   const image = document.querySelector('.loadFileArea');
-  image.src = URL.createObjectURL(event.target.files[0]);
-  console.log(image.src);
+  const button = document.querySelector('.uploadBtn');
+  // const newImage = document.createElement(image);
+  // if ((i = 0)) {
+  //   image.src = URL.createObjectURL(event.target.files[0]);
+  // } else {
+  // }
+  // const multiImage = (i) => {
+  //   for (let i = 0; i < 5; i++) {
+  //     image.src = URL.createObjectURL(event.target.files[i]);
+  //     loadImage.appendChild(newImage);
+  //   }
+  // };
+  // 만약 1개라면 그대로 출력
+  // 2개이상이라면 style바꾸고,file의 개수만큼 image를 태그를 늘려라.
+  image.src = URL.createObjectURL(e.target.files[0]);
+  console.log(e.target.files);
+  button.style.backgroundColor = '#F26E22';
+  // multiImage();
 };
 const textArea = document.querySelector('.uploadBoxText');
 textArea.addEventListener('input', autoResize, false);
@@ -9,3 +26,5 @@ function autoResize() {
   this.style.height = '1px';
   this.style.height = this.scrollHeight + 'px';
 }
+
+// 다중 파일 업로드, 다중 파일 업로드 시 크기 변경
