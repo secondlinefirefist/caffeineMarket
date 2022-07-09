f9ba60ae36b37a1159006a952cfe50b52b1c8e0f
BradleyyKim <win930323@gmail.com>
2022-07-09 21:17:43 +0900

    FE-9 fix : upload.js 기능추가

--- src/js/upload.js
+++ src/js/upload.js
@@ -2,7 +2,9 @@ window.URL = window.URL || window.webkitURL;
 
 const fileSelect = document.getElementById('fileSelect'),
   fileElem = document.getElementById('fileElem'),
-  fileList = document.getElementById('fileList');
+  fileList = document.getElementById('fileList'),
+  inputFile = document.querySelector('.inputDisabled');
+let filesArray;
 
 fileSelect.addEventListener(
   'click',
@@ -15,8 +17,6 @@ fileSelect.addEventListener(
   false
 );
 
-//이미지 가로 스크롤, 여러 파일 업로드
-// 다중 파일 업로드, 다중 파일 업로드 시 크기 변경
 function handleFiles(files) {
   const ul = document.createElement('ul');
   fileList.appendChild(ul);
@@ -24,6 +24,7 @@ function handleFiles(files) {
     const li = document.createElement('li');
     const img = document.createElement('img');
     const deleteImg = document.createElement('div');
+    deleteImg.dataset.id = files[i].name;
     ul.classList.add('imgUl');
     li.classList.add('imgLi');
     deleteImg.classList.add('deleteImg');
@@ -35,6 +36,7 @@ function handleFiles(files) {
       window.URL.revokeObjectURL(this.src);
     };
   }
+  filesArray = Array.from(files);
 }
 // textArea 자동 줄 채우기
 const textArea = document.querySelector('.uploadBoxText');
@@ -45,12 +47,16 @@ function autoResize() {
 }
 
 // x 버튼 추가, 클릭시 이미지 삭제
-
-// 업로드 버튼 클릭 시, POST로 서버에 파일전송 및 피드에 UI 생성
-// 1. x버튼 클릭시, 해당 li 제거 (id값?)
-
+// fileList index 대상 파일 삭제
+// post 시, 삭제된 파일 전송 불가 기능
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('deleteImg')) {
+    const list = [...e.target.parentElement.parentElement.children];
+    const index = list.indexOf(e.target.parentElement);
+    filesArray.splice(index, 1);
     e.target.parentNode.remove();
   }
 });
+
+// 업로드 버튼 클릭 시, POST로 서버에 파일전송 및 피드에 UI 생성
+// 1. x버튼 클릭시, 해당 li 제거 (id값?)
