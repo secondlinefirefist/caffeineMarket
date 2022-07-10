d43a979e92a545616c3250383ae10fe8d5c12c58
jisu-kim <kss2460@naver.com>
2022-07-10 16:54:54 +0900

    Add: error message

--- src/Js/login.js
+++ src/Js/login.js
@@ -1,10 +1,10 @@
 //인풋 내용 검사 후 로그인 버튼 활성화
-const loginInput = document.querySelector('#email');
-const pwInput = document.querySelector('#password');
-const loginButton = document.querySelector('.login-button');
-//const loginInput = document.querySelector('.login');
+const $emailInput = document.querySelector('#email');
+const $pwInput = document.querySelector('#password');
+const $loginButton = document.querySelector('.login-button');
+//const emailInput = document.querySelector('.login');
 
-let checkLoginInput = false;
+let checkemailInput = false;
 let checkpwInput = false;
 // const checkInputValue = () => (event, checkValue) => {
 //   if (event.target.value) {
@@ -13,10 +13,10 @@ let checkpwInput = false;
 //   return checkValue
 // }
 
-const checkLoginInputValue = (event) => {
+const checkEmailInputValue = (event) => {
     if (event.target.value !== '') {
-    checkLoginInput = true;
-    } else checkLoginInput = false;
+    checkemailInput = true;
+    } else checkemailInput = false;
     showButton();
 }
 
@@ -28,37 +28,41 @@ const checkpwInputValue = (event) => {
 }
 
 const showButton = () => {
-  if(checkLoginInput === true && checkpwInput === true) {
-    loginButton.classList.add('focus')
-  } else loginButton.classList.remove('focus')
+  if(checkemailInput === true && checkpwInput === true) {
+    $loginButton.classList.add('focus')
+  } else {
+    $loginButton.classList.remove('focus')
+  }
 }
 
-loginInput.addEventListener('input', checkLoginInputValue);
-pwInput.addEventListener('input', checkpwInputValue);
-loginButton.addEventListener('click', loginData);
+$emailInput.addEventListener('input', checkEmailInputValue);
+$pwInput.addEventListener('input', checkpwInputValue);
+$loginButton.addEventListener('click', loginData);
 
 
 // 로그인 데이터 요청 
 const url = "https://mandarin.api.weniv.co.kr";
-
+const createP = document.createElement('p');
+const createText = document.createTextNode('이거 안맞음')
 
 async function loginData() {
-  try{
-  const res = await fetch(url+"/user/login/", {
-                      method: "POST",
-                      headers: {
-                          "Content-Type": "application/json",
-                      },
-                      body : JSON.stringify({
-                          "user":{
-                              "email": loginInput.value,
-                              "password": pwInput.value
-                          }
-                      })
-                  });
-  const resJson = await res.json();
-  console.log(resJson);
-  } catch(err){
-    console.error(err);
-  }
+    try{
+    const res = await fetch(url+"/user/login/", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body : JSON.stringify({
+                            "user":{
+                                "email": $emailInput.value,
+                                "password": $pwInput.value
+                            }
+                        })
+                    });
+    const resJson = await res.json();
+    console.log(resJson);
+    } catch(err){
+      console.error(err);
+      $pwInput.appendChild(createText);
+    }
 }
