6032e1e4cfe8a4ac13d67c2e67c2c21b4604e7ad
greenT-Hee <heehee970322@gmail.com>
2022-07-11 13:22:54 +0900

    Refactor:delete product list markup

--- src/Js/myProfile.js
+++ src/Js/myProfile.js
@@ -1,3 +1,5 @@
+/*데이터 불러오기*/
+
 /*게시글 좋아요 버튼, 댓글 버튼 클릭 시 배경 채워짐*/
 const btnLike = document.querySelector('#btnLike');
 const btnComment = document.querySelector('#btnComment');
@@ -14,7 +16,7 @@ btnComment.addEventListener('click', () => {
 
 /*상품등록 버튼 누르면 판매 상품 리스트 생성*/
 
-function createProductList(event) {
+const createProductList = () => {
   const productList = document.querySelector('.productList');
   const productItem = document.createElement('li');
   const productItemBtn = document.createElement('button');
@@ -33,7 +35,9 @@ function createProductList(event) {
   productItemSpan.textContent = '애월읍 엄청 큰 낑깡';
   productItemStrong.classList.add('prodcutPrice');
   productItemStrong.textContent = '4500';
-}
+
+  preventReload();
+};
 
 const btnAddProduct = document.querySelector('.btnAddProduct');
 btnAddProduct.addEventListener('click', createProductList);
