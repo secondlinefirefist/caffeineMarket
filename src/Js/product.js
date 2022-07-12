bee2b286eb8f4332908ec25639650af57f650ba6
Jihoon Chae <hoonn94@naver.com>
2022-07-12 15:14:43 +0900

    Add : DOM으로 클래스를 변수선언

---
+++ src/Js/product.js
@@ -0,0 +1,18 @@
+const $btnSave = document.querySelector('.btnSave');
+const $productForm = document.querySelector('.productForm');
+const $inputProductImg = document.querySelector('.inputProductImg');
+const $inputProductTitle = document.querySelector('.inputProductTitle');
+const $inputProductPrice = document.querySelector('.inputProductPrice');
+const $inputProductLink = document.querySelector('.inputProductLink');
+
+// 이미지 업로드
+
+// 상품명 길이 유효성 검사
+
+// 상품명 1자미만 15자 초과시 error 메시지 처리
+
+// 가격에 숫자만 입력 가능하도록
+
+// 가격에 숫자 입력하지 않을 경우 error 메시지 처리
+
+// 이미지, 상품명, 가격, 판매링크 모두 입력될 경우 저장버튼 활성화
