901274cbf2621c5b1dd555225796ce65f451733b
Jihoon Chae <hoonn94@naver.com>
2022-07-14 13:15:44 +0900

    Add : 상품 데이터 POST 요청

--- src/Js/product.js
+++ src/Js/product.js
@@ -44,7 +44,40 @@ const handleCheckInput = () => {
   // 이미지 업로드 되는 경우를 추가해야함
 };
 
-// 이미지, 상품명, 가격, 판매링크 모두 입력될 경우 저장버튼 활성화 이벤트 리스너
-$productForm.addEventListener('input', handleCheckInput);
 
-// POST요청
+// 저장버튼 클릭시 상품 데이터 POST 요청
+const url = 'https://mandarin.api.weniv.co.kr';
+
+async function productData() {
+  try {
+    const res = await fetch(url + '/product', {
+      method: 'POST',
+      body: JSON.stringify({
+        product: {
+          itemName: $inputProductTitle.value,
+          price: parseInt($inputProductPrice.value),
+          link: $inputProductLink.value,
+          itemImage: url,
+        },
+      }),
+      headers: {
+        'Authorization':
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyY2Y3MWJkODJmZGNjNzEyZjQ5YWQxYyIsImV4cCI6MTY2Mjk0NjI1OSwiaWF0IjoxNjU3NzYyMjU5fQ.ZZJU2G6yXYACD1gGnoMa_DPYBDCwD2mOWAsBtf5chxI',
+        'Content-type': 'application/json',
+      },
+    });
+    const resJson = await res.json();
+    console.log(resJson);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// 상품등록 체크로직
+
+// 상품등록 성공시 페이지전환
+
+// 상품등록 식패시 알림문구 출력
+
+$productForm.addEventListener('input', handleCheckInput);
+$btnSave.addEventListener('click', productData);
