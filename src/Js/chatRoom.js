6597af52a7cc495aa78d907238478eb8e22fbb84
Jihoon Chae <hoonn94@naver.com>
2022-07-16 15:38:28 +0900

    Add : 전송버튼 누르면 말풍선 렌더링

--- src/Js/chatRoom.js
+++ src/Js/chatRoom.js
@@ -1,6 +1,7 @@
 const $btnTextSubmit = document.querySelector('.btnTextSubmit');
 const $inputImg = document.querySelector('#inputImg');
 const $inputChatText = document.querySelector('.inputChatText');
+const $chatRoom = document.querySelector('.chatRoom');
 
 // 메시지 입력시 true 반환
 const checkMessage = () => {
@@ -23,8 +24,23 @@ const handleCheckMessage = () => {
   }
 };
 
+// 전송버튼 누르면 말풍선 렌더링
+const handleTextSend = () => {
+  const date = new Date();
+  const time = date.getHours() + ':' + date.getMinutes() + '';
+  $chatRoom.innerHTML += `
+  <li class="chatContent myChatContent">
+  <strong class="chatTime">${time}</strong>
+  <p class="chatText">${$inputChatText.value}</p>
+</li>
+  `;
+  $inputChatText.value = '';
+  handleCheckMessage();
+};
+
 // 이미지 업로드 시 미리보기
 
 // 채팅방 나가기 버튼 클릭 시 채팅 리스트로 이동
 
 $inputChatText.addEventListener('input', handleCheckMessage);
+$btnTextSubmit.addEventListener('click', handleTextSend);
