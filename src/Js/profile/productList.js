ed35390b4c0242a1982ca3e88322ad6143a1c55a
greenT-Hee <heehee970322@gmail.com>
2022-07-17 00:08:03 +0900

    Add: 선택된 상품을 삭제하기

--- src/Js/profile/productList.js
+++ src/Js/profile/productList.js
@@ -30,17 +30,21 @@ function createProductList() {
 
     productList.appendChild(li);
     li.appendChild(button);
+    li.setAttribute('data-id', `${prodcutListDummy[i].id}`);
+
     button.append(img, span, strong);
 
     button.setAttribute('type', 'button');
     button.setAttribute('class', 'btnProductItem');
-    button.setAttribute('value', parseInt([i]));
+    button.setAttribute('data-id', `${prodcutListDummy[i].id}`);
+    console.log(this);
 
     img.setAttribute('src', '#');
     img.setAttribute('alt', '상품이미지');
 
     span.setAttribute('class', 'productTitle');
     span.textContent = `${prodcutListDummy[i].itemName}`;
+
     strong.setAttribute('class', 'productPrice');
     strong.textContent = `${prodcutListDummy[i].price
       .toString()
@@ -57,6 +61,11 @@ function onProductSettingModal() {
     btnProductItem[i].addEventListener('click', (event) => {
       event.stopPropagation();
       productModal.classList.toggle('displayModal');
+      console.log(event.target);
+      btnDelProduct.setAttribute(
+        'dataId',
+        event.currentTarget.getAttribute('data-id')
+      );
     });
   }
   // 메인 눌렀을 때도 모달이 닫힐 수 있게 설정
@@ -70,6 +79,7 @@ function onProductSettingModal() {
 }
 
 // 상품 삭제 마지막 확인 모달
+const btnCancelDelProduct = document.querySelector('#btnCancelDelProduct');
 const btnDelProduct = document.querySelector('#btnDelProduct');
 const subDelProductModal = document.querySelector('#subDelProductModal');
 function openCheckDelProductModal() {
@@ -77,12 +87,17 @@ function openCheckDelProductModal() {
     event.stopPropagation();
     subDelProductModal.classList.toggle('displayModal');
   });
+
+  btnCancelDelProduct.addEventListener('click', () => {
+    subDelProductModal.classList.remove('displayModal');
+  });
 }
 openCheckDelProductModal();
 
 //상품 삭제
-async function okDelProduct() {
-  const productId = prodcutListDummy[0].id;
+async function delProduct() {
+  const productId = btnDelProduct.getAttribute('dataId');
+  // const productId = prodcutListDummy[0].id;
   try {
     const res = await fetch(url + '/product/' + productId, {
       method: 'DELETE',
@@ -93,12 +108,22 @@ async function okDelProduct() {
       body: JSON.stringify(),
     });
     const json = await res.json();
-    confirm(json.message);
-    location.reload();
+    okDelProducItem(json);
   } catch {
     console.error('ERROR!');
   }
 }
 
+function okDelProducItem(json) {
+  alert(json.message);
+  location.reload();
+}
+
 const btnOkDeleteProdcut = document.querySelector('#btnOkDeleteProdcut');
-btnOkDeleteProdcut.addEventListener('click', okDelProduct);
+btnOkDeleteProdcut.addEventListener('click', delProduct);
+
+//상품 등록
+const btnAddProduct = document.querySelector('.btnAddProduct');
+btnAddProduct.addEventListener('click', () => {
+  location.href = '../pages/product.html';
+});
