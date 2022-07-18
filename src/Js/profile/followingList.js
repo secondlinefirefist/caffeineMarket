fa98211a004de2e1bc9ead77b416bde747a88cfb
greenT-Hee <heehee970322@gmail.com>
2022-07-18 15:06:03 +0900

    Add:나의 팔로우 리스트 보여주기

---
+++ src/Js/profile/followingList.js
@@ -0,0 +1,49 @@
+const url = 'https://mandarin.api.weniv.co.kr';
+const accountname = `${window.localStorage.getItem('accountname')}`;
+async function followListData() {
+  try {
+    const res = await fetch(url + '/profile/' + accountname + '/following', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${window.localStorage.getItem('token')}`,
+        'Content-type': 'application/json',
+      },
+    });
+    const resJson = await res.json();
+    console.log(resJson);
+    showfollowingList(resJson);
+  } catch {
+    console.error('ERROR');
+  }
+}
+followListData();
+
+const followersList = document.querySelector('.followersList');
+function showfollowingList(resJson) {
+  console.log(resJson);
+  for (let i = 0; i < resJson.length; i++) {
+    const li = document.createElement('li'),
+      img = document.createElement('img'),
+      div = document.createElement('div'),
+      strong = document.createElement('strong'),
+      p = document.createElement('p'),
+      button = document.createElement('button');
+
+    followersList.append(li);
+    li.append(img, div, button);
+    div.append(strong, p);
+
+    img.setAttribute('src', resJson[i].image);
+    img.setAttribute('alt', '팔로워 프로필사진');
+    img.setAttribute('class', 'followerProfileImg');
+
+    div.setAttribute('class', 'txtWrap');
+    strong.setAttribute('class', 'txtFollowerTitle');
+    strong.textContent = resJson[i].username;
+    p.setAttribute('class', 'txtFollwerInfo');
+    p.textContent = resJson[i].intro;
+
+    button.setAttribute('class', 'btnFollow');
+    button.textContent = '팔로우';
+  }
+}
