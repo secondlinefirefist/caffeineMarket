4eef02b10396821c0d3140c2509444a05ed770cb
greenT-Hee <heehee970322@gmail.com>
2022-07-18 22:27:09 +0900

    Add: 내프로필 팔로우 및 언팔로우 기능 추가

--- src/Js/profile/followingList.js
+++ src/Js/profile/followingList.js
@@ -10,7 +10,6 @@ async function followListData() {
       },
     });
     const resJson = await res.json();
-    console.log(resJson);
     showfollowingList(resJson);
   } catch {
     console.error('ERROR');
@@ -49,5 +48,45 @@ function showfollowingList(resJson) {
     button.setAttribute('class', 'btnFollow');
     button.setAttribute('isfollow', resJson[i].isfollow);
     button.textContent = '취소';
+  }
+  unfollowData(resJson);
+}
+
+function unfollowData(resJson) {
+  console.log(resJson, '언팔로우 함수 내부입니다');
+  let btnFollow = document.querySelectorAll('.btnFollow');
+  for (let i = 0; i < btnFollow.length; i++) {
+    btnFollow[i].addEventListener('click', (event) => {
+      console.log(resJson[i].accountname);
+      let unfollowUserData = resJson[i];
+      let followState = event.currentTarget.getAttribute('isfollow');
+      let targetButton = event.currentTarget;
+      clickUnFollow(unfollowUserData, followState, targetButton);
+    });
+  }
+}
+
+async function clickUnFollow(unfollowUserData, followState, targetButton) {
+  let unfollowUserAccountName = unfollowUserData.accountname;
+  if (followState == 'true') {
+    try {
+      const res = await fetch(
+        url + '/profile/' + unfollowUserAccountName + '/unfollow',
+        {
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${window.localStorage.getItem('token')}`,
+            'Content-type': 'application/json',
+            'body': JSON.stringify(),
+          },
+        }
+      );
+      const resJson = await res.json();
+      targetButton.setAttribute('class', 'btnUnfollow');
+      targetButton.classList.remove('btnFollow');
+      targetButton.textContent = '팔로우';
+    } catch {
+      console.error('ERROR');
+    }
   }
 }
